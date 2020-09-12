112f515e0161313fa2d0f00ec084f6c127eb5a24
SeanAles1 <672018112@student.uksw.edu>
2020-09-13 00:37:19 +0700

    Fix Something

--- src/ConverterFactory.java
+++ src/ConverterFactory.java
@@ -1,14 +1,17 @@
-public class ShapeFactory {
-    public Shape getShape(String shapeType){
-        if (shapeType == null) return null;
-        if (shapeType.equalsIgnoreCase("CIRCLE")){
-            return new Circle();
-        }else if (shapeType.equalsIgnoreCase("RECTANGLE")){
-            return new Rectangle();
-        }else if(shapeType.equalsIgnoreCase("SQUARE")){
-            return new Square();
+public class ConverterFactory {
+    public Converter getShape(String shapeType){
+        if(shapeType == null){
+            return null;
         }
-
+        if(shapeType.equalsIgnoreCase("BINER")){
+            return new Biner();
+        }
+        if(shapeType.equalsIgnoreCase("HEX")){
+            return new Hex();
+        }
+        if(shapeType.equalsIgnoreCase("OKTAL")){
+            return new Oktal();
+        }
         return null;
     }
 }
